d880e3450ad5230f7c7a01200cfd6a103103ede1
AM Hoffman <avamariehoffman@gmail.com>
2019-05-14 19:59:15 -0400

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
 print("hello world")
+
+def about_me(name,
+             preferred_language,
+             favorite_color,
+             pride_and_joy):
+    print("Hi my name is "+name)
+    print("I usually work in "+preferred_language)
+    print("My favorite color is "+favorite_color)
+    print("I'm really proud of "+pride_and_joy)
+
+
